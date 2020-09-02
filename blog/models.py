a9e4d8706237faa3d4f895881efa7bfc1aaa4f6b
Nitin N <nitinnarayanan77@gmail.com>
2020-09-02 13:08:45 +0530

    first commit

---
+++ blog/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+def valcalc(s):
+    value = 0
+    spl = s.split(' ')
+    for x in spl:
+        if x=='xbox':
+            value+=1
+    return value
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    value = models.IntegerField(default=0)
+
+    def __str__(self):
+        return(self.title)
+
+
---
+++ blog/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from queue import PriorityQueue
+from .models import Post,valcalc
+from django.contrib.auth.models import User
+
+pq=PriorityQueue()
+
+posts2 = []
+posts2deq = []
+
+#titl = input("Enter post Title: ")
+#cont = input("Enter post Content: ")
+#user = User.objects.filter(username='Nitin').first()
+#p1 = Post(title=titl,content=cont,author=user,value=valcalc(cont))
+#p1.save()
+
+for post in Post.objects.all():
+   posts2.append([-valcalc(post.content),post.title,post.content,post.author,])
+    
+for x in posts2:
+    pq.put(x)
+
+while not pq.empty():
+    posts2deq.append(pq.get())
+
+def home(request):
+    context = {
+        'posts':Post.objects.all().order_by('-value'),
+        'posts2deq':posts2deq
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html',{'title':'About'})
