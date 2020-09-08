0b5fe8374c780cc46030d528244c8efce5f2c61c
Nitin N <nitinnarayanan77@gmail.com>
2020-09-08 19:55:27 +0530

    settings2

--- django_project/settings.py
+++ django_project/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
