7cf14665a687a7df375e4d999a8a1611753daa5d
Nitin N <nitinnarayanan77@gmail.com>
2020-09-08 20:53:48 +0530

    dep8

--- django_project/settings.py
+++ django_project/settings.py
@@ -121,8 +121,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../blog/static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, '../blog/static'))
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
