9edf4f2869d678fe0a59ac527330d4d7d32cb72c
Nitin N <nitinnarayanan77@gmail.com>
2020-09-08 18:00:25 +0530

    polishing

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
     path('home1/',views.home1,name='blog-home1'),
-    path('post/new/',PostCreateView.as_view(),name='post-create')
+    path('post/new/',PostCreateView.as_view(),name='post-create'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -63,7 +63,7 @@ class PostCreateView(CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-    
+
 
 
 
