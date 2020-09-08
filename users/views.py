40e0975dd6a0e3b6324f5a7fed7a15fbbc7c2c4b
Nitin N <nitinnarayanan77@gmail.com>
2020-09-08 16:25:54 +0530

    login/out

--- users/views.py
+++ users/views.py
@@ -9,8 +9,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Your account has been created! You can now log in')
+            return redirect('login')
 
     else:
         form = UserRegisterForm()
