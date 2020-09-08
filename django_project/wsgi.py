7cd0829ec7d1f65506710fc19145254ceaff6fb9
Nitin N <nitinnarayanan77@gmail.com>
2020-09-08 21:09:07 +0530

    dep8

--- django_project/wsgi.py
+++ django_project/wsgi.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 
 import os
 
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
 
 application = get_wsgi_application()
+
