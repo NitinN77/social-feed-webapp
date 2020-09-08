3b43f5213f44a65290ee0e695fdee71a2e9ba791
Nitin N <nitinnarayanan77@gmail.com>
2020-09-08 12:51:45 +0530

    forms+2sorts

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,9 @@ pq=PriorityQueue()
 posts2 = []
 posts2deq = []
 
+posts2r = []
+posts2rdeq = []
+
 #titl = input("Enter post Title: ")
 #cont = input("Enter post Content: ")
 #user = User.objects.filter(username='Nitin').first()
@@ -16,17 +19,33 @@ posts2deq = []
 
 for post in Post.objects.all():
    posts2.append([-valcalc(post.content),post.title,post.content,post.author,])
+
+for post in Post.objects.all():
+   posts2r.append([valcalc(post.content),post.title,post.content,post.author,])
     
 for x in posts2:
     pq.put(x)
-
+    
 while not pq.empty():
     posts2deq.append(pq.get())
+
+for x in posts2r:
+    pq.put(x)
+    
+while not pq.empty():
+    posts2rdeq.append(pq.get())
 
 def home(request):
     context = {
         'posts':Post.objects.all().order_by('-value'),
         'posts2deq':posts2deq
+    }
+    return render(request, 'blog/home.html', context)
+
+def home1(request):
+    context = {
+        'posts':Post.objects.all().order_by('-value'),
+        'posts2deq':posts2rdeq
     }
     return render(request, 'blog/home.html', context)
 
--- users/views.py
+++ users/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
@@ -13,7 +13,7 @@ def register(request):
             return redirect('blog-home')
 
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
 
