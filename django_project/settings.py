ed25909bbf3311a3c23842b36707a637b6bd7d8a
Nitin N <nitinnarayanan77@gmail.com>
2020-09-08 19:46:32 +0530

    settings mod

--- django_project/settings.py
+++ django_project/settings.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-
+STATIC_URL = os.path.join(BASE_DIR,'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
