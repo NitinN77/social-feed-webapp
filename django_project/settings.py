6653f001be41e095706762e93b933147fc5e9d0b
Nitin N <nitinnarayanan77@gmail.com>
2020-09-08 21:40:08 +0530

    dep8

--- django_project/settings.py
+++ django_project/settings.py
@@ -131,5 +131,5 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'blog/static'),
 )
