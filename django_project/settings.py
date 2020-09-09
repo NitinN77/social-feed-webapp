b007b33431d0164ff1b097926db27e2109820477
Nitin N <nitinnarayanan77@gmail.com>
2020-09-09 06:51:57 +0530

    dep8

--- django_project/settings.py
+++ django_project/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'django_project.urls'
@@ -128,3 +129,7 @@ LOGIN_REDIRECT_URL = 'blog-home'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
