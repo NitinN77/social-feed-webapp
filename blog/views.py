8337b48e8184a7282aade5569901867c6560f640
Nitin N <nitinnarayanan77@gmail.com>
2020-09-09 07:23:40 +0530

    test1

--- blog/views.py
+++ blog/views.py
@@ -4,39 +4,20 @@ from django.views.generic import ListView,CreateView
 from .models import Post,valcalc
 from django.contrib.auth.models import User
 
-pq=PriorityQueue()
-
-posts2 = []
-posts2deq = []
-
-posts2r = []
-posts2rdeq = []
-
-#titl = input("Enter post Title: ")
-#cont = input("Enter post Content: ")
-#user = User.objects.filter(username='Nitin').first()
-#p1 = Post(title=titl,content=cont,author=user,value=valcalc(cont))
-#p1.save()
-
-for post in Post.objects.all():
-   posts2.append([-valcalc(post.content),post.title,post.content,post.author,post.date_posted])
-
-for post in Post.objects.all():
-   posts2r.append([valcalc(post.content),post.title,post.content,post.author,post.date_posted])
-    
-for x in posts2:
-    pq.put(x)
-    
-while not pq.empty():
-    posts2deq.append(pq.get())
-
-for x in posts2r:
-    pq.put(x)
-    
-while not pq.empty():
-    posts2rdeq.append(pq.get())
-
 def home(request):
+    pq=PriorityQueue()
+    posts2 = []
+    posts2deq = []
+
+    for post in Post.objects.all():
+        posts2.append([-valcalc(post.content),post.title,post.content,post.author,post.date_posted])
+
+    for x in posts2:
+        pq.put(x)
+    
+    while not pq.empty():
+        posts2deq.append(pq.get())
+
     context = {
         'posts':Post.objects.all(),
         'posts2deq':posts2deq,
@@ -44,6 +25,19 @@ def home(request):
     return render(request, 'blog/home.html', context)
 
 def home1(request):
+    pq=PriorityQueue()
+    posts2r = []
+    posts2rdeq = []
+
+    for post in Post.objects.all():
+        posts2r.append([valcalc(post.content),post.title,post.content,post.author,post.date_posted])
+
+    for x in posts2r:
+        pq.put(x)
+    
+    while not pq.empty():
+        posts2rdeq.append(pq.get())
+
     context = {
         'posts':Post.objects.all(),
         'posts2deq':posts2rdeq,
