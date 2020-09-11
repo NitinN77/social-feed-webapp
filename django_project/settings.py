4df464b8febd1ac3f03c0004cac1ac48aacd3d7b
Nitin N <nitinnarayanan77@gmail.com>
2020-09-11 10:09:28 +0530

    dbfix

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -130,3 +131,5 @@ LOGIN_REDIRECT_URL = 'blog-home'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
