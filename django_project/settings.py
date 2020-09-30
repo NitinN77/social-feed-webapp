d6deabbe5e37a951a0b11aff697dd326dfb32b9c
Nitin N <nitinnarayanan77@gmail.com>
2020-09-30 08:55:06 +0530

    scrape mode contextbox

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['socialfeeddjango.herokuapp.com','127.0.0.1']
 
